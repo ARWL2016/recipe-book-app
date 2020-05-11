ddb88f19b5549b9869eab1cc09e48734b9d61b79
Alistair Willis <awillis@mttltd.com>
2020-05-11 12:35:14 +0100

    update stack on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path'); 
 
 var app = express(); 
-var port = process.env.PORT || 5000; 
+var port = process.env.PORT || 5001; 
 
 app.use(express.static('app')); 
 
